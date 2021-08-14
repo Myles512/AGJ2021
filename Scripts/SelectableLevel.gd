a83a4145204ed3f2b492a12b70ca21c4ee786e00
Boris <bfleyshe@ualberta.ca>
2021-08-14 14:46:30 -0600

    WIP level selection

---
+++ Scripts/SelectableLevel.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+
+export(int) var levelID
+export(bool) var unlocked = false
+var mouseInBox = false
+signal levelSelected
+signal startLevel
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if mouseInBox:
+			emit_signal("startLevel")
+
+
+func _on_Area2D_mouse_entered():
+	mouseInBox = true
+	if unlocked:
+		emit_signal("levelSelected", levelID)
+
+
+func _on_Area2D_mouse_exited():
+	mouseInBox = false
