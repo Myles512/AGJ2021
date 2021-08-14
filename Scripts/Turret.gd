663110008a8a60cf3d50ca9a0aa3d0e081926e1b
BuildingOutLoud <a_sage_00@hotmail.com>
2021-08-13 21:27:19 -0600

    some laser work

--- Scripts/Turret.gd
+++ Scripts/Turret.gd
@@ -1,15 +1,45 @@
 extends Node2D
 
+#scene settings
+export(PackedScene) var laser_scene
 export var rotate_speed = 0.01
+export var laser_distance = 100
+
+#debug stuff
+export var debug_drawraycast = false
 
 var mouse_in_rotate = false
 var mouse_in_drag = false
-var mouse_clicked = false
 var dragging = false
 var rotating = false
 
+
+
 func _ready():
 	pass
+
+
+
+func _physics_process(delta):
+	laser()
+
+
+
+func laser():
+	var space_state = get_world_2d().direct_space_state
+	var testpoint = Vector2()
+	testpoint += $Laserstart.position
+	testpoint.y -= laser_distance
+	if debug_drawraycast:
+		$DebugLaser.set_point_position(0, $Laserstart.position)
+		$DebugLaser.set_point_position(1, testpoint)
+		$DebugLaser.visible = true
+	var result = space_state.intersect_ray($Laserstart.position, testpoint, [self])
+	print(result.size())
+#	if result.collider != null:
+#		print(result[0].collider.name)
+
+
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -31,15 +61,16 @@ func _input(event):
 
 
 
-
 func _on_PickupArea_mouse_entered():
 	mouse_in_drag = true
+#	print("can drag")
 
 func _on_PickupArea_mouse_exited():
 	mouse_in_drag = false
 
 func _on_RotateArea_mouse_entered():
 	mouse_in_rotate = true
+#	print("can rotate")
 
 func _on_RotateArea_mouse_exited():
-	mouse_in_rotate = true
+	mouse_in_rotate = false
